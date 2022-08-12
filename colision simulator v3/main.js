1e1a05da2318f5b96289ecb3b3f54372f4b16a34
Hydan <90104826+Hydan1625@users.noreply.github.com>
2022-08-12 20:02:36 -0300

    Added outline

--- colision simulator v3/main.js
+++ colision simulator v3/main.js
@@ -7,6 +7,7 @@ let my_circle_radius = 20;
 let max_speed = 12;
 let circles = [];
 let squares = [];
+let light_sources = [];
 let copy = [];
 let new_list = [];
 let lines_toggle = false;
@@ -24,8 +25,8 @@ let ctx = canvas.getContext("2d");
 window.addEventListener("mousemove", setMousePosition, true);
 window.addEventListener("resize", resizeing, true);
 
-canvas.width = innerWidth - 400;
-canvas.height = innerHeight - 100;
+canvas.width = innerWidth - 350; // -400
+canvas.height = innerHeight - 50; // -100
 
 let mouseX = Math.floor(canvas.width / 2);
 let mouseY = Math.floor(canvas.height / 2);
@@ -42,8 +43,8 @@ let $div_table = document.querySelector('.table');
 
 //manipulação de variavel aqui
 
-$div_table.style.height = canvas.height - 10 + "px";
-$color_picker.style.height = $div_table - ($div_table * 0.35) + "px";
+$div_table.style.height = canvas.height + "px";
+//$color_picker.style.height = $div_table - ($div_table * 0.35) + "px";
 $color_picker.value = background_color;
 
 
@@ -67,16 +68,24 @@ function setMousePosition(event) {
 class object_contructor {
 	constructor(type, x, y, speed, color = "black", radius = null, width = null, height = null) {
 		this.type = type;
+		this.light_souce = false;
+
 		this.x = x;
 		this.y = y;
+
 		this.color = color;
+		this.grd = this.color;
+
 		this.speed = speed;
 		this.text = 0;
 		this.dx = 2 * this.speed;
 		this.dy = this.speed;
-		this.dxv = this.speed * 2;
-		this.dyv = this.speed;
-		//this.old_speed = this.speed;
+
+		this.linew_width = 3;
+		this.shadow_color = "black";
+		this.shadow_blur = 20;
+		this.outline = false;
+
 		//this.image_path = "792632.png";
 
 		if (this.type === "square") {
@@ -113,34 +122,92 @@ class object_contructor {
 	}
 
 	draw(ctx) {
+		ctx.shadowBlur = this.shadow_blur;
+		ctx.lineWidth = this.linew_width;
+
+		this.light_souce === true ? ctx.shadowColor = this.color : ctx.shadowColor = this.shadow_color;
+
+
+
 		switch (this.type) {
 			case "square":
 				ctx.beginPath();
-				ctx.lineWidth = 5;
-				ctx.strokeStyle = this.color;
 
-				ctx.fillStyle = this.color;
+				if (this.light_souce === false) {
+
+					if (light_sources.length > 0) {
+
+						light_sources.forEach(element => {
+							this.grd = ctx.createRadialGradient(element.x, element.y, element.radius * element.mass, element.x, element.y, canvas.width);
+
+							this.grd.addColorStop(0, "white")
+							this.grd.addColorStop(0.05, element.color);
+							this.grd.addColorStop(0.15, this.color);
+							this.grd.addColorStop(0.5, "black");
+							this.grd.addColorStop(1, "black");
+
+							element.x > this.x + this.width ? ctx.shadowOffsetX = - 20 : ctx.shadowOffsetX = 20;
+							element.y > this.y + this.height ? ctx.shadowOffsetY = - 20 : ctx.shadowOffsetY = 20;
+
+						});
+					} else { this.grd = this.color }
+
+				} else { this.grd = this.color }
+				//ctx.fillStyle = this.color
+
+				ctx.fillStyle = this.grd;
 				ctx.fillRect(this.x, this.y, this.width, this.height);
-				ctx.rect(this.x, this.y, this.width, this.height);
-				ctx.stroke();
+
+				if (this.outline === true) {
+					ctx.strokeStyle = this.color;
+					ctx.rect(this.x, this.y, this.width, this.height);
+					ctx.stroke();
+					//ctx.fill();
+				}
 				ctx.closePath();
-				//ctx.fill();
 				break;
 
 			case "circle":
 				ctx.beginPath();
-				ctx.fillStyle = this.color;
+
+				if (this.light_souce === false) {
+
+					if (light_sources.length > 0) {
+
+						light_sources.forEach(element => {
+							this.grd = ctx.createRadialGradient(element.x, element.y, element.radius * element.mass, element.x, element.y, canvas.width * 0.5);
+
+							this.grd.addColorStop(0, "white")
+							this.grd.addColorStop(0.15, element.color);
+							this.grd.addColorStop(0.5, this.color);
+							this.grd.addColorStop(1, "black");
+
+							element.x > this.x + this.radius ? ctx.shadowOffsetX = - 20 : ctx.shadowOffsetX = 20;
+							element.y > this.y + this.radius ? ctx.shadowOffsetY = - 20 : ctx.shadowOffsetY = 20;
+
+						});
+					} else { this.grd = this.color }
+
+				} else { this.grd = this.color }
+
+				ctx.fillStyle = this.grd;
 				ctx.strokeStyle = this.color;
+
 				ctx.textAlign = "center";
 				ctx.textBaseline = "middle";
 				ctx.font = Math.floor(this.radius * 0.5) + "px" + " Arial";
 				ctx.fillText(this.text, this.x, this.y);
 
-				ctx.lineWidth = 5;
 				ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-				ctx.stroke();
-				ctx.fillStyle = this.color;
-				ctx.fill();
+				ctx.fillStyle = this.grd;
+
+				if (this.outline === true) {			
+					ctx.stroke();
+					ctx.fill();
+				} else {
+					//ctx.stroke();
+					ctx.fill();
+				}
 				ctx.closePath();
 				break;
 		}
@@ -256,28 +323,29 @@ class object_contructor {
 		}
 	}
 
-	/*
-	reduce_dx(element) {
-		if (this.dx >= max_speed) { this.dx = max_speed }
-		if (this.dx <= -max_speed) { this.dx = -max_speed }
-		if (element.dx >= max_speed) { element.dx = max_speed }
-		if (element.dx <= -max_speed) { element.dx = -max_speed }
-		else {
-			this.dx < 0 ? this.dx += element.mass : this.dx -= element.mass;
-			element.dx < 0 ? element.dx += this.mass : element.dx += this.mass;
+	boundaring_distance(element) {
+		let varx = element.x, vary = element.y;
+
+		if (element.x < this.x) {
+			varx = this.x;
 		}
+
+		if (element.y < this.y) {
+			varx = this.y;
+		}
+
+		if (element.x > this.x + this.width) {
+			varx = this.x + this.width;
+		}
+
+		if (element.y > this.y + this.height) {
+			vary = this.y + this.height;
+		}
+
+		return [varx, vary];
 	}
 
-	reduce_dy(element) {
-		if (this.dy >= max_speed) { this.dy = max_speed }
-		if (this.dy <= -max_speed) { this.dy = -max_speed }
-		if (element.dy >= max_speed) { element.dy = max_speed }
-		if (element.dy <= -max_speed) { element.dy = -max_speed }
-		else {
-			this.dy < 0 ? this.dy += element.mass : this.dy -= element.mass
-			element.dy < 0 ? element.dy += this.mass : element.dy += this.mass
-		}
-	}*/
+
 
 	square_response(values) {
 		//colision_response_with_squares
@@ -296,21 +364,17 @@ class object_contructor {
 						if (vector_y > 0) {
 							this.y = element.bottom;
 							element.dy = -element.dy;
-							//this.reduce_dy(element);
 						} else {
 							this.y = element.y - this.height;
 							element.dy = -element.dy;
-							//this.reduce_dy(element);
 						}
 					} else {
 						if (vector_x > 0) {
 							this.x = element.right;
 							element.dx = -element.dx;
-							//this.reduce_dx(element);
 						} else {
 							this.x = element.x - this.width;
 							element.dx = -element.dx;
-							//this.reduce_dx(element);
 						}
 					}
 					//this.dx = element.dx = this.dy = element.dy = 0;
@@ -369,13 +433,13 @@ class object_contructor {
 								element.y = this.top - element.radius - 1;
 								this.dy = -this.dy;
 								element.dy = -element.dy;
-								//this.reduce_dy(element);
+
 							} else {
 								//this.y -= distance;
 								element.y = this.bottom + element.radius + 1;
 								this.dy = -this.dy;
 								element.dy = -element.dy;
-								//this.reduce_dy(element);
+
 							}
 						} else {
 							if (vectorX > 0) {
@@ -383,13 +447,13 @@ class object_contructor {
 								//this.x += distance;
 								this.dx = -this.dx;
 								element.dx = -element.dx;
-								//this.reduce_dx(element);
+
 							} else {
 								//this.x -= distance;
 								element.x = this.right + element.radius + 1;
 								this.dx = -this.dx;
 								element.dx = -element.dx;
-								//this.reduce_dx(element);
+
 							}
 						}
 					}
@@ -440,13 +504,13 @@ class object_contructor {
 								element.y = this.top - element.radius - 1;
 								this.dy = -this.dy;
 								element.dy = -element.dy;
-								//this.reduce_dy(element);
+
 							} else {
 								//this.y -= distance;
 								element.y = this.bottom + element.radius + 1;
 								this.dy = -this.dy;
 								element.dy = -element.dy;
-								//this.reduce_dy(element);
+
 							}
 						} else {
 							if (vectorX > 0) {
@@ -454,13 +518,13 @@ class object_contructor {
 								//this.x += distance;
 								this.dx = -this.dx;
 								element.dx = -element.dx;
-								//this.reduce_dx(element);
+
 							} else {
 								//this.x -= distance;
 								element.x = this.right + element.radius + 1;
 								this.dx = -this.dx;
 								element.dx = -element.dx;
-								//this.reduce_dx(element);
+
 							}
 						}
 
@@ -492,24 +556,20 @@ class object_contructor {
 							this.y += distance;
 							//this.dy = -this.dy;
 							element.dy = -element.dy;
-							//this.reduce_dy(element);
 						} else {
 							this.y -= distance;
 							//this.dy = -this.dy;
 							element.dy = -element.dy;
-							//this.reduce_dy(element);
 						}
 					} else {
 						if (distanceX < 0) {
 							this.x -= distance;
 							//this.dx = -this.dx;
 							element.dx = -element.dx;
-							//this.reduce_dx(element);
 						} else {
 							this.x += distance;
 							//this.dx = -this.dx;
 							element.dx = -element.dx;
-							//this.reduce_dx(element);
 						}
 					}
 
@@ -537,8 +597,10 @@ let $all_line = document.getElementById("show_all_line").addEventListener('click
 let $square_line = document.getElementById("show_square_line").addEventListener('click', change_toggle_square);
 let $circle_line = document.getElementById("show_circle_line").addEventListener('click', change_toggle_circle);
 
+let $creat_light = document.getElementById("create_light");
+
 let $create_circle = document.getElementById("create_circle");
-$create_circle.onclick = function () {
+$create_circle.onclick = () => {
 	let random_x = Math.floor(Math.random() * canvas.width) + 1;
 	let random_y = Math.floor(Math.random() * canvas.height) + 1;
 	let random_radius = Math.floor(Math.random() * max_size) + 1;
@@ -549,6 +611,20 @@ $create_circle.onclick = function () {
 	random_circle.random_color();
 	circles.push(random_circle);
 };
+
+$creat_light.onclick = () => {
+	let random_x = Math.floor(Math.random() * canvas.width) + 1;
+	let random_y = Math.floor(Math.random() * canvas.height) + 1;
+	let random_radius = Math.floor(Math.random() * max_size) + 1;
+	if (random_radius < 20) { random_radius = 20 }
+	let random_speed = Math.floor(Math.random() * max_speed) + 1;
+
+	let random_circle = new object_contructor("circle", random_x, random_y, random_speed, "purple", random_radius);
+	random_circle.random_color();
+	random_circle.light_souce = true;
+	light_sources.push(random_circle);
+};
+
 
 
 let $create_square = document.getElementById("create_square");
@@ -575,6 +651,10 @@ $delete_square.onclick = function () { squares.pop() };
 let $delete_circle = document.getElementById("delete_circle");
 $delete_circle.onclick = function () { circles.pop() };
 
+let $delete_light = document.getElementById("delete_light").onclick = () => {
+	light_sources.pop()
+}
+
 let $stop = document.getElementById("stop").addEventListener('click', stop_movement);
 let $start = document.getElementById("start").addEventListener('click', start_movement);
 
@@ -582,16 +662,19 @@ let $start = document.getElementById("start").addEventListener('click', start_mo
 
 let my_square = new object_contructor("square", 0, 0, Math.floor(Math.random() * max_speed) + 1, "black", null, 100, 100);
 let my_circle = new object_contructor("circle", 0, 0, Math.floor(Math.random() * max_speed) + 1, "black", my_circle_radius);
-
+let my_light = new object_contructor("circle", mouseX, mouseY, 0, "white", 20)
 
 
 //functions here
 //functions to set up figure and mouse movement
 
-function update_my_circle() {
-	my_circle.update();
-	//my_circle = new Circle(mouseX, mouseY, my_circle_radius, "black", 0, 0);
-	update_position(my_circle);
+function update_my_circle(element) {
+	element.update();
+
+	my_light = new object_contructor("circle", mouseX, mouseY, 0, "white", 30);
+	my_light.light_souce = true;
+	light_sources[0] = my_light;
+	update_position(element);
 }
 
 function update_my_square() {
@@ -621,12 +704,14 @@ function update_position(circle) {
 }
 
 function clear() {
-	if (squares.length != 0 || circles.length != 0) {
+	if (squares.length != 0 || circles.length != 0 || light_sources.length != 0) {
 		let size1 = circles.length;
 		let size2 = squares.length;
-		for (var i = 0; i < Math.max(size1, size2) + 1; i++) {
+		let size3 = light_sources.length
+		for (var i = 0; i < Math.max(size1, size2, size3) + 1; i++) {
 			circles.pop();
 			squares.pop();
+			light_sources.pop();
 		}
 	} else { return }
 }
@@ -644,17 +729,24 @@ function stop_movement() {
 		square.dx = square.dy = 0;
 	} for (circle of circles) {
 		circle.dx = circle.dy = 0;
+	} for (circle of light_sources) {
+		circle.dx = circle.dy = 0;
 	}
 }
 
 function start_movement() {
 	let len1 = circles.length
 	let len2 = squares.length
+	let len3 = light_sources.length
 	for (element of circles) {
 		let random_speed = Math.floor(Math.random() * max_speed) + 1
 		element.dx = element.dy = random_speed
 	}
 	for (element of squares) {
+		let random_speed = Math.floor(Math.random() * max_speed) + 1
+		element.dx = element.dy = random_speed
+	}
+	for (element of light_sources) {
 		let random_speed = Math.floor(Math.random() * max_speed) + 1
 		element.dx = element.dy = random_speed
 	}
@@ -666,6 +758,15 @@ function start_movement() {
 
 let $create_my_circle = document.getElementById("create_my_circle");
 let $crete_my_square = document.getElementById("create_my_square");
+let $create_my_light = document.getElementById('create_my_light');
+let $show_outline = document.getElementById("show_outline").onclick = () => {
+	circles.forEach(i => {
+		i.outline = !i.outline
+	});
+	squares.forEach(i => {
+		i.outline = !i.outline
+	});
+}
 
 let $clear_canvas = document.getElementById("clear_all");
 
@@ -681,12 +782,36 @@ $create_my_circle.onclick = function () {
 	circles.push(my_circle);
 };
 
-
+$create_my_light.onclick = function () {
+	let my_circle = new object_contructor("circle", mouseX, mouseY, 0, "white", 30);
+	my_light.light_souce = true;
+	light_sources.push(my_circle);
+};
 
 function game() {
 
-
 	canvas.style.background = $color_picker.value;
+
+
+	//update_my_circle(my_light);
+
+	light_sources.forEach((element) => {
+
+		let grd = ctx.createRadialGradient(element.x, element.y, element.radius * element.mass, element.x, element.y, canvas.width * 0.5);
+
+		grd.addColorStop(0, element.color);
+		grd.addColorStop(0.3, $color_picker.value);
+		grd.addColorStop(0.85, $color_picker.value);
+		grd.addColorStop(1, "black");
+
+		ctx.fillStyle = grd;
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+		element.circle_response(light_sources);
+		element.circle_response(circles);
+		element.update();
+		//element.random_color();
+	});
 
 	my_circle.circle_square_response(squares);
 	my_circle.circle_response(circles);
@@ -701,10 +826,12 @@ function game() {
 
 	$clear_canvas.onclick = () => { clear() };
 
+
 	squares.forEach((element) => {
 
 		element.square_response(squares);
 		element.circle_square_response(circles);
+		element.circle_square_response(light_sources);
 		element.update();
 		//element.random_color();
 	});
@@ -712,9 +839,12 @@ function game() {
 	circles.forEach((element) => {
 
 		element.circle_response(circles);
+		element.circle_response(light_sources);
 		element.update();
 		//element.random_color();
 	});
+
+
 }
 
 let update_frame = function () {
